f1fa850934bcd5b4818f3f468ee283be942c5bbc
michaelmichio <michaelmichio.work@gmail.com>
2023-03-21 12:17:11 +0700

    add sort for graham scan

--- IpeCG/src/CG/Algorithm/GrahamScan.java
+++ IpeCG/src/CG/Algorithm/GrahamScan.java
@@ -1,31 +1,65 @@
 package CG.Algorithm;
 
 import CG.Object.Point;
+import Ipe.Object.Layer;
 import Ipe.Object.Use;
 
 import java.util.ArrayList;
 
 public class GrahamScan {
-    ArrayList<Point> vertices = new ArrayList<>();
+    public ArrayList<Layer> layers = new ArrayList<>();
+    public ArrayList<Point> points = new ArrayList<>();
+    public Point initialPoint;
+
 
     public GrahamScan(ArrayList<Use> uses) {
         if(uses.size() >= 3) {
-            setVertices(uses);
-            sortVertices();
-            // ...
+            setPoints(uses);
+            sortPoints();
+            generateLayers();
         }
         else {
             System.out.println("requires at least 3 points");
         }
     }
 
-    public void sortVertices() {
+    public void generateLayers() {
 
     }
 
-    public void setVertices(ArrayList<Use> uses) {
+    public int getInitialPointIndex() {
+        double min = Double.MAX_VALUE;
+        int idx = 0;
+        for(int i = 0; i < points.size(); i++) {
+            if(points.get(i).y < min) {
+                min = points.get(i).y;
+                idx = i;
+            }
+        }
+        return idx;
+    }
+
+    public void sortPoints() {
+        int initialPointIndex = getInitialPointIndex();
+        initialPoint = points.get(initialPointIndex);
+        points.remove(initialPointIndex);
+        ArrayList<Point> pointsTemp = new ArrayList<>(points);
+        pointsTemp.sort((a, b) -> {
+            double cotanA = -(a.x - initialPoint.x) / (a.y - initialPoint.y);
+            double cotanB = -(b.x - initialPoint.x) / (b.y - initialPoint.y);
+            if (cotanA - cotanB < 0) {
+                return -1;
+            }
+            return 1;
+        });
+        points.clear();
+        points.addAll(pointsTemp);
+        pointsTemp.clear();
+    }
+
+    public void setPoints(ArrayList<Use> uses) {
         for (Use u : uses) {
-            vertices.add(new Point(Double.parseDouble(u.pos.x), Double.parseDouble(u.pos.y)));
+            points.add(new Point(Double.parseDouble(u.pos.x), Double.parseDouble(u.pos.y)));
         }
     }
 }
